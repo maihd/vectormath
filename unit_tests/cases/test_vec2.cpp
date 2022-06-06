dcb0c0d09de3f4e791c0e21026856160052834cf
maihd <maihd.dev@gmail.com>
2022-06-06 18:35:36 +0700

    add more tests for vec2

--- unit_tests/cases/test_vec2.cpp
+++ unit_tests/cases/test_vec2.cpp
@@ -1,8 +1,48 @@
 #include "../test_framework.h"
 
-DEFINE_UNIT_TEST("Test vec2")
+DEFINE_UNIT_TEST("Test vec2 assigment")
 {
-    const vec2 a = vec2_new(0.0f, 0.0f);
+    const vec2 a = vec2_new(1.0f, 2.0f);
     const vec2 b = a;
-    TEST(vec2_equal(a, b));
+    TEST(a.x == b.x && a.y == b.y);
 }
+
+DEFINE_UNIT_TEST("Test vec2 operator neg")
+{
+    const vec2 a = vec2_new(1.0f, 2.0f, 3.0f);
+    const vec2 b = -a;
+    TEST(a.x == -b.x && a.y == -b.y);
+}
+
+DEFINE_UNIT_TEST("Test vec2 operator+")
+{
+    const vec2 a = vec2_new(1.0f, 2.0f);
+    const vec2 b = vec2_new(3.0f, 4.0f);
+    const vec2 c = a + b;
+    TEST(c.x == 4.0f && c.y == 6.0f);
+}
+
+DEFINE_UNIT_TEST("Test vec2 operator-")
+{
+    const vec2 a = vec2_new(1.0f, 2.0f);
+    const vec2 b = vec2_new(3.0f, 6.0f);
+    const vec2 c = a - b;
+    TEST(c.x == -2.0f && c.y == -4.0f);
+}
+
+DEFINE_UNIT_TEST("Test vec2 operator*")
+{
+    const vec2 a = vec2_new(1.0f, 2.0f);
+    const vec2 b = vec2_new(3.0f, 4.0f);
+    const vec2 c = a * b;
+    TEST(c.x == 3.0f && c.y == 8.0f);
+}
+
+DEFINE_UNIT_TEST("Test vec2 operator/")
+{
+    const vec2 a = vec2_new(1.0f, 2.0f);
+    const vec2 b = vec2_new(3.0f, 4.0f);
+    const vec2 c = a / b;
+    TEST(c.x == 1.0f / 3.0f && c.y == 0.5f);
+}
+
