5b6930fa945016bc70ef0189162cc695e60d6dde
maihd <maihd.dev@gmail.com>
2022-09-16 12:41:49 +0700

    update premake5.vectormath.lua

--- premake5.vectormath.lua
+++ premake5.vectormath.lua
@@ -1,26 +1,47 @@
+local function getincludedir(directory)
+    local includedir = path.join(directory, "include")
+    return includedir
+end
+
+local function getsubincludedir(directory)
+    local includedir = getincludedir(directory)
+    local subincludedir = path.join(includedir, "vectormath")
+    return subincludedir
+end
+
 return {
+    -- Add vectormath's include directory to project
+    -- @param: directory - where vectormath is locate
     includedirs = function (directory)
         includedirs {
-            path.join(directory, "include")
+            getincludedir(directory)
         }
 
-        files {
-            path.join(directory, "vectormath_types.natvis"),
-        }
+        filters { "actions:vs*"}
+        do
+            files {
+                path.join(directory, "vectormath_types.natvis"),
+            }
+        end
     end,
 
+    -- Import vectormath's files to project
+    -- @param: directory - where vectormath is locate
     files = function (directory)
+        local includedir = getincludedir(directory)
+        local subincludedir = getsubincludedir(directory)
+
         files {
-            path.join(directory, "include/vectormath/sse_to_neon.h"),
-            path.join(directory, "include/vectormath/stdmath_extensions.h"),
+            path.join(subincludedir, "sse_to_neon.h"),
+            path.join(subincludedir, "stdmath_extensions.h"),
     
-            path.join(directory, "include/vectormath/vectormath_types.h"),
+            path.join(subincludedir, "vectormath_types.h"),
     
-            path.join(directory, "include/vectormath/vectormath_scalar.h"),
-            path.join(directory, "include/vectormath/vectormath_simd.h"),
-            path.join(directory, "include/vectormath/vectormath_operators.h"),
+            path.join(subincludedir, "vectormath_scalar.h"),
+            path.join(subincludedir, "vectormath_simd.h"),
+            path.join(subincludedir, "vectormath_operators.h"),
     
-            path.join(directory, "include/vectormath.h"),
+            path.join(includedir, "vectormath.h"),
         }
     end
 }
