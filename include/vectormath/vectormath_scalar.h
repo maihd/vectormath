45373a118b110054dc3d5342c17f86e9ce2ce078
maihd <maihd.dev@gmail.com>
2022-10-19 00:52:28 +0700

    add vec3 swizzle functions

--- include/vectormath/vectormath_scalar.h
+++ include/vectormath/vectormath_scalar.h
@@ -156,6 +156,136 @@ __forceinline vec2 vec2_yx(vec2 v)
 	return vec2_new(v.y, v.x);
 }
 
+__forceinline vec3 vec3_xxx(vec3 v)
+{
+	return vec3_new(v.x, v.x, v.x);
+}
+
+__forceinline vec3 vec3_xxy(vec3 v)
+{
+	return vec3_new(v.x, v.x, v.y);
+}
+
+__forceinline vec3 vec3_xxz(vec3 v)
+{
+	return vec3_new(v.x, v.x, v.z);
+}
+
+__forceinline vec3 vec3_xyx(vec3 v)
+{
+	return vec3_new(v.x, v.y, v.x);
+}
+
+__forceinline vec3 vec3_xyy(vec3 v)
+{
+	return vec3_new(v.x, v.y, v.y);
+}
+
+__forceinline vec3 vec3_xzx(vec3 v)
+{
+	return vec3_new(v.x, v.z, v.x);
+}
+
+__forceinline vec3 vec3_xzy(vec3 v)
+{
+	return vec3_new(v.x, v.z, v.y);
+}
+
+__forceinline vec3 vec3_xzz(vec3 v)
+{
+	return vec3_new(v.x, v.z, v.z);
+}
+
+__forceinline vec3 vec3_yyy(vec3 v)
+{
+	return vec3_new(v.y, v.y, v.y);
+}
+
+__forceinline vec3 vec3_yyx(vec3 v)
+{
+	return vec3_new(v.y, v.y, v.x);
+}
+
+__forceinline vec3 vec3_yyz(vec3 v)
+{
+	return vec3_new(v.y, v.y, v.z);
+}
+
+__forceinline vec3 vec3_yxx(vec3 v)
+{
+	return vec3_new(v.y, v.x, v.x);
+}
+
+__forceinline vec3 vec3_yxy(vec3 v)
+{
+	return vec3_new(v.y, v.x, v.y);
+}
+
+__forceinline vec3 vec3_yxz(vec3 v)
+{
+	return vec3_new(v.y, v.x, v.z);
+}
+
+__forceinline vec3 vec3_yzx(vec3 v)
+{
+	return vec3_new(v.y, v.z, v.x);
+}
+
+__forceinline vec3 vec3_yzy(vec3 v)
+{
+	return vec3_new(v.y, v.z, v.y);
+}
+
+__forceinline vec3 vec3_yzz(vec3 v)
+{
+	return vec3_new(v.y, v.z, v.z);
+}
+
+__forceinline vec3 vec3_zzz(vec3 v)
+{
+	return vec3_new(v.z, v.z, v.z);
+}
+
+__forceinline vec3 vec3_zzx(vec3 v)
+{
+	return vec3_new(v.z, v.z, v.x);
+}
+
+__forceinline vec3 vec3_zzy(vec3 v)
+{
+	return vec3_new(v.z, v.z, v.y);
+}
+
+__forceinline vec3 vec3_zxx(vec3 v)
+{
+	return vec3_new(v.z, v.x, v.x);
+}
+
+__forceinline vec3 vec3_zxy(vec3 v)
+{
+	return vec3_new(v.z, v.x, v.y);
+}
+
+__forceinline vec3 vec3_zxz(vec3 v)
+{
+	return vec3_new(v.z, v.x, v.z);
+}
+
+__forceinline vec3 vec3_zyx(vec3 v)
+{
+	return vec3_new(v.z, v.y, v.x);
+}
+
+__forceinline vec3 vec3_zyy(vec3 v)
+{
+	return vec3_new(v.z, v.y, v.y);
+}
+
+__forceinline vec3 vec3_zyz(vec3 v)
+{
+	return vec3_new(v.z, v.y, v.z);
+}
+
 // -------------------------------------------------------------
 // Operators-like functions
 // -------------------------------------------------------------
--- unit_tests/cases/test_vec3.cpp
+++ unit_tests/cases/test_vec3.cpp
@@ -46,9 +46,193 @@ DEFINE_UNIT_TEST("Test vec3 operator/")
     TEST(c.x == 0.25f && c.y == 0.4f && c.z == 0.5f);
 }
 
-//DEFINE_UNIT_TEST("Test vec3_cos")
-//{
-//    const vec3 a = simd_sinf_cosf(vec3_new(1.0f, 2.0f, 3.0f));
-//    const vec3 b = vec3_new(4.0f, 5.0f, 6.0f);
-//    TEST(c.x == 0.25f && c.y == 0.4f && c.z == 0.5f);
-//}
+DEFINE_UNIT_TEST("vec3 vec3_xxx(vec3 v)")
+{
+    const vec3 a = vec3_new(1.0f, 2.0f, 3.0f);
+    const vec3 b = vec3_xxx(a);
+    TEST(a.x == b.x && a.x == b.y && a.x == b.z);
+}
+
+DEFINE_UNIT_TEST("vec3 vec3_xxy(vec3 v)")
+{
+	const vec3 a = vec3_new(1.0f, 2.0f, 3.0f);
+	const vec3 b = vec3_xxy(a);
+	TEST(a.x == b.x && a.x == b.y && a.y == b.z);
+}
+
+DEFINE_UNIT_TEST("vec3 vec3_xxz(vec3 v)")
+{
+	const vec3 a = vec3_new(1.0f, 2.0f, 3.0f);
+	const vec3 b = vec3_xxz(a);
+	TEST(a.x == b.x && a.x == b.y && a.z == b.z);
+}
+
+DEFINE_UNIT_TEST("vec3 vec3_xyx(vec3 v)")
+{
+	const vec3 a = vec3_new(1.0f, 2.0f, 3.0f);
+	const vec3 b = vec3_xyx(a);
+	TEST(a.x == b.x && a.y == b.y && a.x == b.z);
+}
+
+DEFINE_UNIT_TEST("vec3 vec3_xyy(vec3 v)")
+{
+	const vec3 a = vec3_new(1.0f, 2.0f, 3.0f);
+	const vec3 b = vec3_xyy(a);
+	TEST(a.x == b.x && a.y == b.y && a.y == b.z);
+}
+
+DEFINE_UNIT_TEST("vec3 vec3_xzx(vec3 v)")
+{
+	const vec3 a = vec3_new(1.0f, 2.0f, 3.0f);
+	const vec3 b = vec3_xzx(a);
+	TEST(a.x == b.x && a.z == b.y && a.x == b.z);
+}
+
+DEFINE_UNIT_TEST("vec3 vec3_xzy(vec3 v)")
+{
+	const vec3 a = vec3_new(1.0f, 2.0f, 3.0f);
+	const vec3 b = vec3_xzy(a);
+	TEST(a.x == b.x && a.z == b.y && a.y == b.z);
+}
+
+DEFINE_UNIT_TEST("vec3 vec3_xzz(vec3 v)")
+{
+	const vec3 a = vec3_new(1.0f, 2.0f, 3.0f);
+	const vec3 b = vec3_xzz(a);
+	TEST(a.x == b.x && a.z == b.y && a.z == b.z);
+}
+
+DEFINE_UNIT_TEST("vec3 vec3_yyy(vec3 v)")
+{
+	const vec3 a = vec3_new(1.0f, 2.0f, 3.0f);
+	const vec3 b = vec3_yyy(a);
+	TEST(a.y == b.x && a.y == b.y && a.y == b.z);
+}
+
+DEFINE_UNIT_TEST("vec3 vec3_yyx(vec3 v)")
+{
+	const vec3 a = vec3_new(1.0f, 2.0f, 3.0f);
+	const vec3 b = vec3_yyx(a);
+	TEST(a.y == b.x && a.y == b.y && a.x == b.z);
+}
+
+DEFINE_UNIT_TEST("vec3 vec3_yyz(vec3 v)")
+{
+	const vec3 a = vec3_new(1.0f, 2.0f, 3.0f);
+	const vec3 b = vec3_yyz(a);
+	TEST(a.y == b.x && a.y == b.y && a.z == b.z);
+}
+
+DEFINE_UNIT_TEST("vec3 vec3_yxx(vec3 v)")
+{
+	const vec3 a = vec3_new(1.0f, 2.0f, 3.0f);
+	const vec3 b = vec3_yxx(a);
+	TEST(a.y == b.x && a.x == b.y && a.x == b.z);
+}
+
+DEFINE_UNIT_TEST("vec3 vec3_yxy(vec3 v)")
+{
+	const vec3 a = vec3_new(1.0f, 2.0f, 3.0f);
+	const vec3 b = vec3_yxy(a);
+	TEST(a.y == b.x && a.x == b.y && a.y == b.z);
+}
+
+DEFINE_UNIT_TEST("vec3 vec3_yxz(vec3 v)")
+{
+	const vec3 a = vec3_new(1.0f, 2.0f, 3.0f);
+	const vec3 b = vec3_yxz(a);
+	TEST(a.y == b.x && a.x == b.y && a.z == b.z);
+}
+
+DEFINE_UNIT_TEST("vec3 vec3_yzx(vec3 v)")
+{
+	const vec3 a = vec3_new(1.0f, 2.0f, 3.0f);
+	const vec3 b = vec3_yxz(a);
+	TEST(a.y == b.x && a.x == b.y && a.z == b.z);
+}
+
+DEFINE_UNIT_TEST("vec3 vec3_yzy(vec3 v)")
+{
+	const vec3 a = vec3_new(1.0f, 2.0f, 3.0f);
+	const vec3 b = vec3_yzy(a);
+	TEST(a.y == b.x && a.z == b.y && a.y == b.z);
+}
+
+DEFINE_UNIT_TEST("vec3 vec3_yxz(vec3 v)")
+{
+	const vec3 a = vec3_new(1.0f, 2.0f, 3.0f);
+	const vec3 b = vec3_yxz(a);
+	TEST(a.y == b.x && a.x == b.y && a.z == b.z);
+}
+
+DEFINE_UNIT_TEST("vec3 vec3_yzz(vec3 v)")
+{
+	const vec3 a = vec3_new(1.0f, 2.0f, 3.0f);
+	const vec3 b = vec3_yzz(a);
+	TEST(a.y == b.x && a.z == b.y && a.z == b.z);
+}
+
+DEFINE_UNIT_TEST("vec3 vec3_zzz(vec3 v)")
+{
+	const vec3 a = vec3_new(1.0f, 2.0f, 3.0f);
+	const vec3 b = vec3_zzz(a);
+	TEST(a.z == b.x && a.z == b.y && a.z == b.z);
+}
+
+DEFINE_UNIT_TEST("vec3 vec3_zzx(vec3 v)")
+{
+	const vec3 a = vec3_new(1.0f, 2.0f, 3.0f);
+	const vec3 b = vec3_zzx(a);
+	TEST(a.z == b.x && a.z == b.y && a.x == b.z);
+}
+
+DEFINE_UNIT_TEST("vec3 vec3_zzy(vec3 v)")
+{
+	const vec3 a = vec3_new(1.0f, 2.0f, 3.0f);
+	const vec3 b = vec3_zzy(a);
+	TEST(a.z == b.x && a.z == b.y && a.y == b.z);
+}
+
+DEFINE_UNIT_TEST("vec3 vec3_zxx(vec3 v)")
+{
+	const vec3 a = vec3_new(1.0f, 2.0f, 3.0f);
+	const vec3 b = vec3_zxx(a);
+	TEST(a.z == b.x && a.x == b.y && a.x == b.z);
+}
+
+DEFINE_UNIT_TEST("vec3 vec3_zxy(vec3 v)")
+{
+	const vec3 a = vec3_new(1.0f, 2.0f, 3.0f);
+	const vec3 b = vec3_zxy(a);
+	TEST(a.z == b.x && a.x == b.y && a.y == b.z);
+}
+
+DEFINE_UNIT_TEST("vec3 vec3_zxz(vec3 v)")
+{
+	const vec3 a = vec3_new(1.0f, 2.0f, 3.0f);
+	const vec3 b = vec3_zxz(a);
+	TEST(a.z == b.x && a.x == b.y && a.z == b.z);
+}
+
+DEFINE_UNIT_TEST("vec3 vec3_zyx(vec3 v)")
+{
+	const vec3 a = vec3_new(1.0f, 2.0f, 3.0f);
+	const vec3 b = vec3_zyx(a);
+	TEST(a.z == b.x && a.y == b.y && a.x == b.z);
+}
+
+DEFINE_UNIT_TEST("vec3 vec3_zyy(vec3 v)")
+{
+	const vec3 a = vec3_new(1.0f, 2.0f, 3.0f);
+	const vec3 b = vec3_zyy(a);
+	TEST(a.z == b.x && a.y == b.y && a.y == b.z);
+}
+
+DEFINE_UNIT_TEST("vec3 vec3_zyz(vec3 v)")
+{
+	const vec3 a = vec3_new(1.0f, 2.0f, 3.0f);
+	const vec3 b = vec3_zyz(a);
+	TEST(a.z == b.x && a.y == b.y && a.z == b.z);
+}
+
+//! LEAVE AN EMPTY LINE HERE, REQUIRE BY GCC/G++
