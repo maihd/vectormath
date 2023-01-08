71d96095d73f63f12dce68e78e5af4ebc235ed56
maihd <maihd.dev@gmail.com>
2023-01-08 08:30:06 +0700

    remove *.exe and *.ilk in scripts folder

--- unit_tests/test_framework.h
+++ unit_tests/test_framework.h
@@ -4,7 +4,7 @@
 #include <assert.h>
 #include <stdlib.h>
 
-#include "../VectorMath.h"
+#include <vectormath.h>
 
 // ------------------------------------------------------------------------------------------
 // Unit tests API
