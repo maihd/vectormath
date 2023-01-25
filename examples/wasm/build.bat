7d51e9f2bd3c53d003baa7b9bf065cb29f802616
maihd <maihd.dev@gmail.com>
2023-01-26 01:25:26 +0700

    window and graphics context

--- examples/wasm/build.bat
+++ examples/wasm/build.bat
@@ -1,28 +1,23 @@
 @echo off
 
+:: Variables
+set EXE_FOLDER=%~dp0
+set ROOT_FOLDER=%EXE_FOLDER%\..\..
+set DIST_FOLDER=%EXE_FOLDER%\dist
+
+:: Build flags
+set OUTPUT=%DIST_FOLDER%\index.html
+set INCDIR=-I%ROOT_FOLDER%\include
+set CFLAGS=-O3
+set LFLAGS=-lGL -sUSE_SDL=2 -sWASM=1
+
+:: Setup environment
 set EMSDK_QUIET=1
 call emsdk_env.bat
 
-if not exist dist (
-    mkdir dist
+:: Start build
+if not exist %DIST_FOLDER% (
+    mkdir %DIST_FOLDER%
 )
 
-set UNIT_TEST_FILES=
-
-:: Comment below lines to remove unit tests
-set UNIT_TEST_FILES=%UNIT_TEST_FILES% ../../unit_tests/cases/test_int32.cpp
-set UNIT_TEST_FILES=%UNIT_TEST_FILES% ../../unit_tests/cases/test_mat4.cpp
-set UNIT_TEST_FILES=%UNIT_TEST_FILES% ../../unit_tests/cases/test_math_ex.cpp
-set UNIT_TEST_FILES=%UNIT_TEST_FILES% ../../unit_tests/cases/test_uint32.cpp
-set UNIT_TEST_FILES=%UNIT_TEST_FILES% ../../unit_tests/cases/test_vec2.cpp
-set UNIT_TEST_FILES=%UNIT_TEST_FILES% ../../unit_tests/cases/test_vec3.cpp
-set UNIT_TEST_FILES=%UNIT_TEST_FILES% ../../unit_tests/cases/test_vec4.cpp
-set UNIT_TEST_FILES=%UNIT_TEST_FILES% ../../unit_tests/cases/test_vec4_swizzles.cpp
-
-:: Build flags
-set OUTPUT=dist/index.html
-set INCDIR=-I../../include
-set CFLAGS=-O3
-set LFLAGS=-lGL -s USE_SDL=2 -s WASM=1
-
-call emcc main.cpp %UNIT_TEST_FILES% %INCDIR% -o %OUTPUT% %CFLAGS% %LFLAGS%
+call emcc %EXE_FOLDER%\main.cpp %INCDIR% -o %OUTPUT% %CFLAGS% %LFLAGS%
--- examples/wasm/main.cpp
+++ examples/wasm/main.cpp
@@ -1,13 +1,69 @@
+#include <time.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-#define RUN_UNIT_TESTS
-#define UNIT_TESTS_MAIN run_all_unit_tests
-#include "../../unit_tests/test_framework.h"
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_opengl.h>
+
+#include <GLES2/gl2.h>
+#include <emscripten.h>
+
+static void main_loop();
+
+SDL_Window*     window;
+SDL_Renderer*   renderer;
+SDL_GLContext   context;
 
 int main(int argc, const char* argv[])
 {
-    printf("Hello world from VectorMath!\n");
-    return run_all_unit_tests(argc, argv);
+    SDL_CreateWindowAndRenderer(1280, 720, SDL_WINDOW_OPENGL, &window, &renderer);
+    // SDL_Init(SDL_INIT_VIDEO);
+
+    // SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
+    // SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
+    // SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+
+    // window = SDL_CreateWindow("Window", SDL_WINDOWPOS_UNDEFINED,
+    //     SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_OPENGL);
+
+    context = SDL_GL_CreateContext(window);
+    if (context == nullptr)
+    {
+        printf("Context is null :(\n");
+        printf("%s\n", SDL_GetError());
+
+        return -1;
+    }
+
+    emscripten_set_main_loop(main_loop, 60, 1);
+    return 0;
+}
+
+void main_loop()
+{
+    srand((unsigned int)time(nullptr));
+
+    SDL_Event event;
+    while (SDL_PollEvent(&event))
+    {
+        if (event.type == SDL_QUIT)
+        {
+            SDL_GL_DeleteContext(context);
+            SDL_DestroyWindow(window);
+            SDL_Quit();
+
+            context = nullptr;
+            window = nullptr;
+            break;
+        }
+
+        SDL_GL_MakeCurrent(window, context);
+        
+        glClearColor(0.0, 0.0, 0.0, 1);
+        glClear(GL_COLOR_BUFFER_BIT);
+        
+        SDL_GL_SwapWindow(window);
+    }
 }
 
 //! LEAVE AN EMPTY LINE HERE, REQUIRE BY GCC/G++
