910a415b74a72eff2c8a3c3ec516ffba1e6af789
maihd <maihd.dev@gmail.com>
2023-04-28 02:53:01 +0700

    mat4_look_at -> mat4_lookat

--- include/vectormath/vectormath_scalar.h
+++ include/vectormath/vectormath_scalar.h
@@ -1938,7 +1938,7 @@ __forceinline mat4 mat4_perspective(float fovRadians, float aspect, float znear,
     );
 }
 
-__forceinline mat4 mat4_look_at(vec3 eye, vec3 target, vec3 up)
+__forceinline mat4 mat4_lookat(vec3 eye, vec3 target, vec3 up)
 {
     const vec3 z = vec3_normalize(vec3_sub(eye, target));
     const vec3 x = vec3_normalize(vec3_cross(vec3_normalize(up), z));
