1bc132c251d9c3f8f05d2616ad2e0156c5a819c2
maihd <maihd.dev@gmail.com>
2024-06-06 10:39:04 +0700

    Add constructors docs for C++

--- include/vectormath/vectormath_constructors.h
+++ include/vectormath/vectormath_constructors.h
@@ -113,8 +113,34 @@ __forceinline vec4 __vec4_copy(vec4 v)
 //
 
 #ifdef __cplusplus
+/// Create new 2D vector
+/// Overloading:
+///     - vec2():           2D vector with 2 components as zero
+///     - vec2(float s):    2D vector with 2 components equals to 's'
+///     - vec2(float x, float y, float z)
+///     - vec2(vec2):       dummy cast
+///     - vec2(vec3):       cast vec3 to vec2
+///     - vec2(vec4):       cast vec4 to vec2
 #define vec2(...) vec2_new(__VA_ARGS__)
+
+
+/// Create new 3D vector
+/// Overloading:
+///     - vec3():           3D vector with 3 components as zero
+///     - vec3(float s):    3D vector with 3 components equals to 's'
+///     - vec3(float x, float y, float z)
+///     - vec3(vec3):       dummy cast
+///     - vec3(vec2):       cast vec2 to vec3
+///     - vec3(vec4):       cast vec4 to vec3
 #define vec3(...) vec3_new(__VA_ARGS__)
+
+
+/// Create new 4D vector
+/// Overloading:
+///     - vec4():           4D vector with 4 components as zero
+///     - vec4(float s):    4D vector with 4 components equals to 's'
+///     - vec4(float x, float y, float z, float w)
+///     - vec4(vec4):       dummy cast
 #define vec4(...) vec4_new(__VA_ARGS__)
 #else
 
